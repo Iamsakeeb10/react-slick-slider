a81c38700709f604bca41cb343e77e5b187d3f9a
Shakib Ahmed <shakibshovon.10@gmail.com>
2025-01-02 23:38:38 +0600

    did some cleanup

--- src/components/SliderComponent.tsx
+++ src/components/SliderComponent.tsx
@@ -2,54 +2,12 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import { slides } from "../data/data";
+import { settings } from "../data/static/staticPortion";
 import "./SliderComponent.css";
 
-// Custom Previous Arrow
-// Custom Previous Arrow
-
-function SliderComponent() {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
-
+const SliderComponent = () => {
   return (
-    <div
-      style={{
-        background: "#ccc",
-        width: "100%",
-        height: "100vh",
-      }}
-    >
+    <div className="container">
       <div className="carousel-container">
         <h1 className="carousel-title">Our Featured Slides</h1>
         <Slider {...settings}>
@@ -65,23 +23,11 @@ function SliderComponent() {
           ))}
         </Slider>
       </div>
-      <div
-        style={{
-          position: "absolute",
-          bottom: "30px",
-          right: "10px",
-        }}
-      >
-        <p
-          style={{
-            fontSize: 18,
-          }}
-        >
-          Crafted with ❤️ by Shakib
-        </p>
+      <div className="floating-container">
+        <p className="floating-text">Crafted with ❤️ by Shakib</p>
       </div>
     </div>
   );
-}
+};
 
 export default SliderComponent;
