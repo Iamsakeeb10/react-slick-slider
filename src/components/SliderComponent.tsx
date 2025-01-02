3787255a4420373ba7d345ca2795c690d667fe69
Shakib Ahmed <shakibshovon.10@gmail.com>
2025-01-02 23:42:42 +0600

    did

--- src/components/SliderComponent.tsx
+++ src/components/SliderComponent.tsx
@@ -2,10 +2,43 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import { slides } from "../data/data";
-import { settings } from "../data/static/staticPortion";
 import "./SliderComponent.css";
 
 const SliderComponent = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <div className="container">
       <div className="carousel-container">
